e594e6bfbb6b39a8a4a091ba51b6d49a66caef07
TreeTrum <sam@sjd.co>
2018-11-08 21:46:37 +1100

    Updates

--- webpack.config.js
+++ webpack.config.js
@@ -7,70 +7,70 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const imageminMozjpeg = require('imagemin-mozjpeg');
 
-module.exports = env => {
-	return {
-		mode: 'development',
-		entry: ['./src/js/index.js', './src/scss/main.scss'],
-		output: {
-			path: path.resolve(__dirname, 'dist'),
-			filename: 'bundle.js'
-		},
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /(node_modules|bower_components)/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['@babel/preset-env', '@babel/preset-react'],
-							plugins: ['@babel/plugin-proposal-class-properties']
-						}
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+	mode: 'development',
+	entry: ['./src/js/index.js', './src/scss/main.scss'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+						plugins: ['@babel/plugin-proposal-class-properties']
 					}
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						env.production ? 'style-loader' : MiniCssExtractPlugin.loader,
-						'css-loader',
-						{
-							loader: 'postcss-loader',
-							options: {
-								plugins: [autoPrefixer({ browsers: ['> 1%', 'last 2 versions'] })]
-							}
-						},
-						'sass-loader'
-					]
 				}
-			]
-		},
-		plugins: [
-			new BrowserSyncPlugin({
-				port: devConfig.port,
-				proxy: devConfig.url,
-				files: ['**/*.php'],
-				reloadDelay: 0
-			}),
-			new MiniCssExtractPlugin({
-				filename: '[name].css',
-				chunkFilename: '[id].css'
-			}),
-			new CopyWebpackPlugin([
-				{
-					from: 'src/images/',
-					to: 'images'
-				}
-			]),
-			new ImageminPlugin({
-				test: /\.(jpe?g|png|gif|svg)$/i,
-				options: {},
-				plugins: [
-					imageminMozjpeg({
-						quality: 80,
-						progressive: true
-					})
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							plugins: [autoPrefixer({ browsers: ['> 1%', 'last 2 versions'] })]
+						}
+					},
+					'sass-loader'
 				]
-			})
+			}
 		]
-	};
+	},
+	plugins: [
+		new BrowserSyncPlugin({
+			port: devConfig.port,
+			proxy: devConfig.url,
+			files: ['**/*.php'],
+			reloadDelay: 0
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css'
+		}),
+		new CopyWebpackPlugin([
+			{
+				from: 'src/images/',
+				to: 'images'
+			}
+		]),
+		new ImageminPlugin({
+			test: /\.(jpe?g|png|gif|svg)$/i,
+			options: {},
+			plugins: [
+				imageminMozjpeg({
+					quality: 80,
+					progressive: true
+				})
+			]
+		})
+	]
 };
